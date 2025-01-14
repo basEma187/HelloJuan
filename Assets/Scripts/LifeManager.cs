02bf5bf550faa8feb836dff657d7a58455fbf2b6
Emanuele <emanuele.bassis@intre.it>
2025-01-14 15:46:04 +0100

    Add HealthBar management

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeManager : MonoBehaviour
 {
     public int life;
+
+    public Image bar;
+
+    private int _maxLife;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _maxLife = life;
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateBar();
         if (life <= 0)
             gameObject.SetActive(false);
     }
@@ -22,6 +29,10 @@ public class LifeManager : MonoBehaviour
     {
         life += delta;
     }
-    
-    
+
+    private void UpdateBar()
+    {
+        var lifePercentage = (1.0f * life) / _maxLife;
+        bar.fillAmount = lifePercentage;
+    }
 }
