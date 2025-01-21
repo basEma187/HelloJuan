b0737129d656178ee0d5ead3d732cb1a6584c6e8
Emanuele <emanuele.bassis@intre.it>
2025-01-21 14:48:36 +0100

    Add movement animation

--- Assets/Scripts/Chase.cs
+++ Assets/Scripts/Chase.cs
@@ -10,6 +10,7 @@ public class Chase : MonoBehaviour
     private float _distance;
     private NavMeshAgent _enemy;
     private float _currentInterval;
+    private Animator _animator;
 
     public float interval;
     
@@ -21,6 +22,7 @@ public class Chase : MonoBehaviour
         _player = GameObject.Find("Player");
 
         _enemy =  GetComponent<NavMeshAgent>();
+        _animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,15 +31,24 @@ public class Chase : MonoBehaviour
         _distance = Vector3.Distance(_player.transform.position, transform.position);
         Debug.DrawLine(transform.position,_player.transform.position,Color.green);
 
-        if (_distance <= 5.0f)
+        if (_distance <= 5.0f && _player.activeInHierarchy)
+        {
+            _animator.SetBool("Run",true);
             _enemy.destination = _player.transform.position;
+        }
         else
+        {
+            _animator.SetBool("Run",false);
             _enemy.destination = transform.position;
+        }
+            
 
         _currentInterval -= Time.deltaTime;
         if (_currentInterval <= 0.0f && _distance <= 2.0f  && _player.activeInHierarchy)
         {
             Debug.Log(_player.GetComponent<LifeManager>().life);
+            _animator.SetBool("Run",false);
+            _animator.SetTrigger("Punch");
             _player.GetComponent<LifeManager>().AddLife(-5);
             _currentInterval = interval;
         }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,11 +7,17 @@ public class PlayerMove : MonoBehaviour
     public float speed = 3.0F;
     public float rotateSpeed = 3.0F;
     private float _distance;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
 
     void Update()
     {
         var enemy = GameObject.Find("Enemy");
-        if(enemy)
+        if (enemy)
             _distance = Vector3.Distance(enemy.transform.position, transform.position);
         CharacterController controller = GetComponent<CharacterController>();
 
@@ -20,16 +27,54 @@ public class PlayerMove : MonoBehaviour
         // Move forward / backward
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speed * Input.GetAxis("Vertical");
+        
+        ManageMovementAnimation();
         controller.SimpleMove(forward * curSpeed);
+        
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if ( _distance <= 2.0f && enemy)
+            _animator.SetTrigger("Punch");
+            if (_distance <= 2.0f && enemy)
             {
                 enemy.GetComponent<LifeManager>().AddLife(-5);
                 Debug.Log("pugno");
             }
+        }
+    }
+
+    private void ManageMovementAnimation()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            _animator.SetBool("RunBack",false);
+            _animator.SetBool("Run",true);
             
+        }
+        else if (Input.GetAxis("Vertical") < 0) 
+        {
+            _animator.SetBool("Run",false);
+            _animator.SetBool("RunBack", true);
+        }
+        else
+        {
+            _animator.SetBool("Run",false);
+            _animator.SetBool("RunBack",false);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            _animator.SetBool("LeftRun",false);
+            _animator.SetBool("RightRun",true);
+        }else if (Input.GetAxis("Horizontal") < 0)
+        {
+            _animator.SetBool("RightRun",false);
+            _animator.SetBool("LeftRun",true);
+        }
+        else
+        {
+            _animator.SetBool("RightRun",false);
+            _animator.SetBool("LeftRun",false);
         }
     }
 }
